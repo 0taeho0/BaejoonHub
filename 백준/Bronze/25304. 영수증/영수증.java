8e7e9983ed66b2e1ddd40ef0fdfb0941acb4ae48
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-06-16 17:41:07 +0900

    [Bronze IV] Title: 영수증, Time: 232 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int X = scan.nextInt();
+		int N = scan.nextInt();
+		int temp = 0;
+		
+		for(int i=0; i < N; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			temp += a * b;
+		}
+		if(X == temp) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		scan.close();
+	}
+}
