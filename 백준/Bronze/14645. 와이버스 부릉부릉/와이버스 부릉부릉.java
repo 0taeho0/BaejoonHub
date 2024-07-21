c89e1952ba6ac1a229f5878452b235a2c71220e6
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-21 13:43:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 204 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	
+    	int N = scan.nextInt();
+    	int K = scan.nextInt();
+    	
+    	int [] arr = new int[N];
+    	
+    	for(int i=0; i<arr.length; i++) {
+    		int A = scan.nextInt();
+    		int B = scan.nextInt();
+    	}
+    	System.out.println("비와이");
+    }
+}
