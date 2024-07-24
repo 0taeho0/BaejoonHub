7858249cd98e7f9118b53fb45162a28474f967cb
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-24 14:55:48 +0900

    [Bronze V] Title: Serca, Time: 536 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,6 @@
+a = [' @@@   @@@ ', '@   @ @   @', '@    @    @', '@         @'
+         ,' @       @ ' ,'  @     @  ' ,'   @   @   ' ,'    @ @    ','     @     ']
+
+for i in range(int(input())):
+    for j in range(len(a)):
+        print(a[j])
