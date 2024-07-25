db04f38d2ba0adb628e203ec8e48fec3b2195525
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-25 17:33:04 +0900

    [Bronze IV] Title: 주사위, Time: 548 ms, Memory: 25624 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int N = scan.nextInt();
+       for(int i=0; i<N; i++) {
+    	   int a = scan.nextInt();
+    	   int b = scan.nextInt();
+    	   
+    	   System.out.printf("Case %d: %d\n",i+1, a+b);
+       }
+    } 
+}
