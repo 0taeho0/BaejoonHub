311d513cf6f48105f6617b80f054ae5c9f39f334
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-28 18:15:52 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 200 ms, Memory: 17664 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -7,13 +7,18 @@ public class Main {
         String name = "SciComLove";
         int N = scan.nextInt();
         
-        for(int i=0; i<N; i++) {
-            String n = "";
-        	for(int j=name.length(); j>0; j--) {
-        		n += name.substring(j-1, j);
-        	}
-        	name = n;
+        if(N%2 == 0) {
+        	System.out.println(name);
+        }else {
+        	for(int i=0; i<1; i++) {
+                String n = "";
+            	for(int j=name.length(); j>0; j--) {
+            		n += name.substring(j-1, j);
+            	}
+            	name = n;
+            }
+            System.out.println(name);	
         }
-        System.out.println(name);
+        
 	}
 }
