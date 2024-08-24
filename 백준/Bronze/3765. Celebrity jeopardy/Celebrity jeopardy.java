9875febbfabdf8775c5c44f09c7533293ac8d918
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-08-24 14:40:55 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 96 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	while(true) {
+    		String n = bf.readLine();
+    		
+    		if(n == null || n.equals("")) break;
+    		
+    		sb.append(n).append("\n");
+    	}
+    	System.out.println(sb);
+    }
+}
