32b0bde3b1ce9f4b8242085b30329fdeae8fcc8f
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-08-25 22:03:43 +0900

    [Bronze IV] Title: Next in line, Time: 164 ms, Memory: 17376 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	Scanner scan = new Scanner(System.in);
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int A = scan.nextInt();
+    	int B = scan.nextInt();
+    	
+    	System.out.println(B-A + B);
+	}
+}
